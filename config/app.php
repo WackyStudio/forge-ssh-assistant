1dc3a943b0278692aa8f07b4dcf75ad9fe478ead
Thomas Nørgaard <tgn@wackystudio.com>
2021-05-31 09:53:03 +0200

    [WIP] upgrading laravel zero

--- config/app.php
+++ config/app.php
@@ -1,8 +1,60 @@
-<?php return [
-    'name'       => 'Forge SSH Assistant',
-    'version'    => 'unreleased',
-    'production' => false,
-    'providers'  => [
-        'App\\Providers\\AppServiceProvider',
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Name
+    |--------------------------------------------------------------------------
+    |
+    | This value is the name of your application. This value is used when the
+    | framework needs to place the application's name in a notification or
+    | any other location as required by the application or its packages.
+    |
+    */
+
+    'name' => 'Forge SSH Assistant',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Version
+    |--------------------------------------------------------------------------
+    |
+    | This value determines the "version" your application is currently running
+    | in. You may want to follow the "Semantic Versioning" - Given a version
+    | number MAJOR.MINOR.PATCH when an update happens: https://semver.org.
+    |
+    */
+
+    'version' => app('git.version'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Environment
+    |--------------------------------------------------------------------------
+    |
+    | This value determines the "environment" your application is currently
+    | running in. This may determine how you prefer to configure various
+    | services the application utilizes. This can be overridden using
+    | the global command line "--env" option when calling commands.
+    |
+    */
+
+    'env' => 'development',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Autoloaded Service Providers
+    |--------------------------------------------------------------------------
+    |
+    | The service providers listed here will be automatically loaded on the
+    | request to your application. Feel free to add your own services to
+    | this array to grant expanded functionality to your applications.
+    |
+    */
+
+    'providers' => [
+        App\Providers\AppServiceProvider::class,
     ],
+
 ];
